f34ec121fb9cc47739fa277ce20a2e3a6dfa5f7a
Wanderson ferreira <wandersonwfs7@gmail.com>
2017-09-15 16:01:46 -0300

    Concertando classe ocorrencia policial dao

--- src/dao/TipoOcorrenciaPolicialDAO.java
+++ src/dao/TipoOcorrenciaPolicialDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import database.Conexao;
+import model.BaseOcorrencia;
 import model.BaseTipoOcorrencia;
 
 import java.sql.PreparedStatement;
@@ -106,26 +107,56 @@ public class TipoOcorrenciaPolicialDAO {
 
     public static ArrayList<BaseTipoOcorrencia> buscarOcorrenciaPolicial() {
 
-            ArrayList<BaseTipoOcorrencia> ocorrenciasPoliciais = new ArrayList<>();
-            String sql = "SELECT id, tipo, descricao FROM tipo_ocorrencias_policiais";
+        ArrayList<BaseTipoOcorrencia> ocorrenciasPoliciais = new ArrayList<>();
+        String sql = "SELECT id, tipo, descricao FROM tipo_ocorrencias_policiais";
 
-            Conexao conexao = new Conexao();
-            try {
-                Statement ps = conexao.conectar().createStatement();
-                ps.execute(sql);
-                ResultSet rs = ps.getResultSet();
-                while (rs.next()) {
-                    BaseTipoOcorrencia ocorrenciaPolicial = new BaseTipoOcorrencia();
-                    ocorrenciaPolicial.setId(rs.getInt("id"));
-                    ocorrenciaPolicial.setTipo(rs.getString("tipo"));
-                    ocorrenciaPolicial.setDescricao(rs.getString("descricao"));
-                    ocorrenciasPoliciais.add(ocorrenciaPolicial);
-                }
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-            } finally {
-                conexao.desconectar();
+        Conexao conexao = new Conexao();
+        try {
+            Statement ps = conexao.conectar().createStatement();
+            ps.execute(sql);
+            ResultSet rs = ps.getResultSet();
+            while (rs.next()) {
+                BaseTipoOcorrencia ocorrenciaPolicial = new BaseTipoOcorrencia();
+                ocorrenciaPolicial.setId(rs.getInt("id"));
+                ocorrenciaPolicial.setTipo(rs.getString("tipo"));
+                ocorrenciaPolicial.setDescricao(rs.getString("descricao"));
+                ocorrenciasPoliciais.add(ocorrenciaPolicial);
             }
-            return ocorrenciasPoliciais;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            conexao.desconectar();
+        }
+        return ocorrenciasPoliciais;
+
+
     }
+    public static ArrayList<BaseOcorrencia> retornarOcorrenciaBombeiro(){
+        ArrayList<BaseOcorrencia> tabelaOcorrenciaPolicial = new ArrayList<>();
+        String sql = "SELECT id, id_tipo_ocorrencias_bombeiros, id_emissor, cep, rua, numero_residencia, logradouro FROM ocorrencias_policiais";
+        Conexao conexao = new Conexao();
+        try {
+            Statement stt = conexao.conectar().createStatement();
+            stt.execute(sql);
+            ResultSet rs = stt.getResultSet();
+            while (rs.next()){
+                BaseOcorrencia ocorrenciaPolicial = new BaseOcorrencia();
+                ocorrenciaPolicial.setId(rs.getInt("id"));
+                ocorrenciaPolicial.setBaseTipoOcorrencia(TipoOcorrenciaPolicialDAO.buscarOPPorID(rs.getInt("id_tipo_ocorrencias_policiais")));
+                //Tipo de ocorrencia
+                ocorrenciaPolicial.setEmissor(EmissorDAO.buscarEmissorPorID(rs.getInt("id_emissor")));
+                ocorrenciaPolicial.setCep(rs.getInt("cep"));
+                ocorrenciaPolicial.setRua(rs.getString("rua"));
+                ocorrenciaPolicial.setNumeroResidencia(rs.getInt("numero_residencia"));
+                ocorrenciaPolicial.setLogradouro(rs.getString("logradouro"));
+                tabelaOcorrenciaPolicial.add(ocorrenciaPolicial);
+            }
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }finally {
+            conexao.desconectar();
+        }
+        return tabelaOcorrenciaPolicial;
+    }
+
 }
--- web/tela_receptor_policial.jsp
+++ web/tela_receptor_policial.jsp
@@ -1,4 +1,6 @@
-<%--
+<%@ page import="model.BaseOcorrencia" %>
+<%@ page import="dao.OcorrenciaPolicialDAO" %>
+<%@ page import="java.util.ArrayList" %><%--
   Created by IntelliJ IDEA.
   User: 98930
   Date: 14/09/2017
@@ -11,15 +13,30 @@
     <title>Ocorrencias Policiais</title>
 </head>
 <body>
-<thead>
-<tr>
-    <td>Código</td>
-    <td>Ocorrencia</td>
-    <td>Endereço</td>
-    <td>Emissor</td>
+<table>
+    <thead>
+    <tr>
+        <th>Código</th>
+        <th>Ocorrencia</th>
+        <th>Endereço</th>
+        <th>Emissor</th>
+    </tr>
+    </thead>
 
+
+
+<tbody>
+<% ArrayList<BaseOcorrencia> ocorrencias = OcorrenciaPolicialDAO.retornarOcorrenciaPolicial(); %>
+<% for (BaseOcorrencia ocorrenciaPolicial: ocorrencias) {%>
+<tr>
+    <td><%= ocorrenciaPolicial.getId()%></td>
+    <td><%= ocorrenciaPolicial.getBaseTipoOcorrencia()%></td>
+    <td><%= ocorrenciaPolicial.endereco()%></td>
+    <td><%= ocorrenciaPolicial.getEmissor()%></td>
 </tr>
-</thead>
+<% } %>
+</tbody>
+</table>
 
 </body>
 </html>
